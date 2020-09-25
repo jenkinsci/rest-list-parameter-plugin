3ddfaeee47d7a2af457f2a76664ff296c1efd5d1
h1dden-da3m0n <33120068+h1dden-da3m0n@users.noreply.github.com>
2020-09-25 15:28:25 +0200

    add some very basic integration tests for the RestValueService

--- src/test/java/io/jenkins/plugins/restlistparam/RestValueServiceTest.java
+++ src/test/java/io/jenkins/plugins/restlistparam/RestValueServiceTest.java
@@ -2,15 +2,38 @@ package io.jenkins.plugins.restlistparam;
 
 import io.jenkins.plugins.restlistparam.logic.RestValueService;
 import io.jenkins.plugins.restlistparam.model.MimeType;
+import io.jenkins.plugins.restlistparam.model.ResultContainer;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collection;
+
+// TODO more unit tests here (preferably replace integration tests)
 public class RestValueServiceTest {
   @Test
-  public void getTest()
-  {
-    RestValueService
-      .get("http://api.github.com/repos/jellyfin/jellyfin/tags?per_page=3", null, MimeType.APPLICATION_JSON, "$.*.name", "%%%");
-    Assert.assertTrue(true);
+  public void successfulGetIntegrationTest() {
+    ResultContainer<Collection<String>> test = RestValueService
+      .get("http://api.github.com/repos/jellyfin/jellyfin/tags?per_page=3",
+           null,
+           MimeType.APPLICATION_JSON,
+           "$.*.name",
+           null);
+    Assert.assertFalse(test.getErrorMsg().isPresent());
+    Assert.assertEquals(3, test.getValue().size());
+    Assert.assertArrayEquals(new String[]{"v10.6.4", "v10.6.3", "v10.6.2"}, test.getValue().toArray());
+  }
+
+  @Test
+  public void unsuccessfulGetIntegrationTest() {
+    ResultContainer<Collection<String>> test = RestValueService
+      .get("https://gitlab.example.com/api/v4/projects/gitlab-org%2Fgitlab-runner/releases",
+           null,
+           MimeType.APPLICATION_JSON,
+           "$.*.tag_name",
+           null);
+    Assert.assertNotNull(test);
+    Assert.assertTrue(test.getErrorMsg().isPresent());
+    Assert.assertEquals(0, test.getValue().size());
+    Assert.assertArrayEquals(new String[]{}, test.getValue().toArray());
   }
 }
